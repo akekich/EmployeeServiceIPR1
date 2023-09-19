418c597fe7215b3806634dc594995c3fd01a3ba0
akekich <49408994+akekich@users.noreply.github.com>
2023-09-19 19:53:46 +0300

    Qsort alg

--- handlers/employee-handler.go
+++ handlers/employee-handler.go
@@ -26,12 +26,14 @@ func NewHandler(employees repo.EmployeeRepo) HandlerEmployee {
 func (e HandlerEmployee) AddHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
 	}
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println("Ошибка чтения тела запроса")
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	employee := models.Employee{}
@@ -39,38 +41,40 @@ func (e HandlerEmployee) AddHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(fmt.Sprintf("Ошибка %s", err.Error()))
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	e.repo.AddEmployee(employee)
 }
 
 func (e HandlerEmployee) GetHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
 	}
 
 	employees := e.repo.GetEmployees()
 	if len(employees) == 0 {
 		w.WriteHeader(http.StatusNotFound)
+		return
 	}
 
 	vars := mux.Vars(r)
 	sortName := vars["sort"]
 	sortedEmployees := []models.Employee{}
+	employeesWithNumber := []models.Employee{}
+	employeesWithoutNumber := []models.Employee{}
+
+	for _, employee := range employees {
+		if employee.EmployeeNumber != nil {
+			employeesWithNumber = append(employeesWithNumber, employee)
+		} else {
+			employeesWithoutNumber = append(employeesWithoutNumber, employee)
+		}
+	}
 
 	switch sortName {
 	case "buble":
-		employeesWithNumber := []models.Employee{}
-		employeesWithoutNumber := []models.Employee{}
-
-		for _, employee := range employees {
-			if employee.EmployeeNumber != nil {
-				employeesWithNumber = append(employeesWithNumber, employee)
-			} else {
-				employeesWithoutNumber = append(employeesWithoutNumber, employee)
-			}
-		}
-
 		length := len(employeesWithNumber)
 		fmt.Println(length)
 		for i := 0; i < (length - 1); i++ {
@@ -83,13 +87,43 @@ func (e HandlerEmployee) GetHandler(w http.ResponseWriter, r *http.Request) {
 
 		sortedEmployees = append(sortedEmployees, employeesWithoutNumber...)
 		sortedEmployees = append(sortedEmployees, employeesWithNumber...)
+	case "quick":
+		low := 0
+		high := len(employeesWithNumber) - 1
+		quickSort(employeesWithNumber, low, high)
+		sortedEmployees = append(employeesWithoutNumber, employeesWithNumber...)
 	}
 
 	jsonData, err := json.Marshal(sortedEmployees)
 	if err != nil {
 		fmt.Println("Ошибка заджисонивания")
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	_, _ = w.Write(jsonData)
+}
+
+func quickSort(arr []models.Employee, low int, high int) {
+	if low < high {
+		pi := partion(arr, low, high)
+
+		quickSort(arr, low, pi-1)
+		quickSort(arr, pi+1, high)
+	}
+}
+
+func partion(arr []models.Employee, low int, high int) int {
+	pivot := arr[high]
+	i := low - 1
+
+	for j := low; j < high; j++ {
+		if *arr[j].EmployeeNumber < *pivot.EmployeeNumber {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return i + 1
 }
